16b3749d3a83ac035ffff38cf7e945c65898c2c5
Rohan Ganguly <rohan.ganguly@snhu.edu>
2018-05-10 22:57:03 +0000

    Adding the calculator program

---
+++ calculator.rb
@@ -0,0 +1,38 @@
+def add(n1+n2)
+    return(n1+n2)
+end
+
+def subtract (n1,n2)
+    return (n1-n2)
+end
+
+def multiply (n1,n2)
+    return (n1*n2)
+end
+
+def divide (n1,n2)
+    return (n1/n2)
+end
+
+print ("Enter first number: ")
+num1=Float(gets)
+
+print ("Enter second number: ")
+num2 = Float(gets)
+
+print ("Enter operation (+,-,*,/)")
+operation = gets.chomp
+
+if operation=="+"
+    puts (add(num1,num2))
+elsif operation == "-"
+    puts (subtract(num1,num2))
+elsif operation =="*"
+    puts (multiply(num1,num2))
+elsif operation == "/"
+    puts (divide(num1,num2)
+else
+    puts ("bad operator")
+end
+
+    
