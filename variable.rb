e225a29002c88eaa501a207449a4bc41ea4f7f42
Rohan Ganguly <rohan.ganguly@snhu.edu>
2018-05-10 12:33:02 +0000

    adding variable.rb

---
+++ variable.rb
@@ -0,0 +1,28 @@
+
+#Change the value of the variable named var3 to make the following statement evaluate to true.
+var1 = true
+var2 = false
+var3 = true
+
+statement = var1 && var2 || var3
+
+#What is the result of the following expression
+
+x = 1
+
+y = 2
+
+z = y
+
+puts z + 1
+
+3
+
+#Write a program to calculate the area of any triangle using the variable names base, height, and area. Keep in mind that the formula to calculate the area of a triangle is 0.5 * ( base * height). Make sure that the result is printed to the screen.
+
+# Replace all instances of ??? with appropriate values or expressions
+
+base =1
+height = 2
+area = 0.5*(base*height)
+puts area
