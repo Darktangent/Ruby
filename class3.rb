f8f2a7372a0f09726c3c4234659ef4d91b693cd9
Rohan Ganguly <rohan.ganguly@snhu.edu>
2018-05-11 23:45:06 +0000

    Add class3.rb

---
+++ class3.rb
@@ -0,0 +1,27 @@
+#super class for types of shapes
+class Shape
+    
+    def initialize(x,y)
+        @x =x
+        @y=y
+    end
+    
+    attr_reader :x, :y
+    attr_writer :x,:y
+    
+    def to_s
+        print("x: ", x, "y", y)
+    end
+    #move method to change x,y cordinates
+    def move(x,y)
+        @x +=x
+        @y +=y
+    end
+end
+
+s1=Shape.new(10,12)
+puts (s1.to_s)
+s1.move(5,2)
+puts (s1.to_s)
+
+
