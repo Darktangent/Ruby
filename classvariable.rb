37adbd51f2ac1acb0721364c9c8686a0473a6787
Rohan Ganguly <rohan.ganguly@snhu.edu>
2018-05-11 21:33:36 +0000

    add classvariable.rb

---
+++ classvariable.rb
@@ -0,0 +1,27 @@
+class Name
+    
+    @@count=0
+    def initialize (first,middle,last)
+        @first=first
+        @middle+middle
+        @last=last
+        @@count +=1
+    end
+    
+    attr_reader :first, :middle, :last
+    attr_writer :first, :middle, :last
+    
+    def to_s
+        print (@last + ", " + @first + " " + @middle)
+    end
+    
+    #creating a class method to return the number of times a new class was created
+    
+    def self.count
+        return @@count
+    end
+end
+aName=Name.new("Jane","Liz", "Smith")
+print Name.count
+print "\n"
+anotherName = Name.new("John","Barrett", "Jones")
